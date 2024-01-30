86de962afa62030484467e617728c5530112b7b1
marcionavarro <marcionavarrodearaujo@gmail.com>
2024-01-29 22:13:07 -0300

    Query Loading State, Query Error State, React Query Devtools

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,37 @@
+import { useQuery } from 'react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'; 
+import axios from 'axios';
 import './App.css'
 
-function App() {
 
-  return (
-    <div>Hello React Query!</div>
+function Pokemon() {
+  const queryInfo = useQuery('pokemon', async () => {
+    await new Promise(resolve => setTimeout(resolve, 1000));
+
+    return axios
+      .get('https://pokeapi.co/api/v2/pokemon')
+      .then(res => res.data.results)
+  })
+
+  return queryInfo.isLoading ? (
+    'Loading...'
+  ) : queryInfo.isError ? (
+    queryInfo.error.message
+  ) : (
+    <div>
+      {queryInfo?.data.map((result) => {
+        return <div key={result.name}>{result.name}</div>
+      })}
+    </div>
   )
 }
 
-export default App
+function App() {
+  return (
+    <div>
+        <Pokemon />
+    </div>
+  )
+}
+
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'; 
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+      <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
+    </QueryClientProvider>
+    
   </React.StrictMode>,
 )
+
