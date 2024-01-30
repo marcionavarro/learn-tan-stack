f96727f4d08f1c20597f2bd292134e8c0b7a5c1f
marcionavarro <marcionavarrodearaujo@gmail.com>
2024-01-29 23:21:12 -0300

    Using Props and State in Queries, Disabling Queries, Multi-part Query Keys

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,32 @@
 import { useQuery } from 'react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import axios from 'axios';
 import './App.css'
+import { useState } from 'react';
 
 function App() {
+  const [pokemon, setPokemon] = useState('');
   return (
     <div>
-      <Count />
-      <Pokemon />
+      <input value={pokemon} onChange={(e) => setPokemon(e.target.value)} />
+      <PokemonSearch pokemon={pokemon} />
     </div>
   )
 }
 
-function usePokemon() {
-  return useQuery('pokemons', async () => {
-    await new Promise(resolve => setTimeout(resolve, 1000));
-    return axios
-      .get('https://pokeapi.co/api/v2/pokemon')
-      .then(res => res.data.results)
-  })
-}
 
-function Count() {
-  const queryInfo = usePokemon();
-  return <h2>You are looking at {queryInfo.data?.length} pokemon</h2>
-}
-
-function Pokemon() {
-  const queryInfo =  usePokemon();
+function PokemonSearch({ pokemon }) {
+  const queryInfo = useQuery(
+    pokemon,
+    async () => {
+      await new Promise(resolve => setTimeout(resolve, 1000));
+      return axios
+        .get(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
+        .then(res => res.data)
+    },
+    {
+      enabled: true,
+    }
+  );
 
   return queryInfo.isLoading ? (
     'Loading...'
@@ -35,9 +34,11 @@ function Pokemon() {
     queryInfo.error.message
   ) : (
     <div>
-      {queryInfo?.data.map((result) => {
-        return <div key={result.name}>{result.name}</div>
-      })}
+      {queryInfo.data?.sprites?.front_default ? (
+        <img src={queryInfo.data.sprites.front_default} alt='pokemon' />
+      ) : (
+        'Pokemon not found.'
+      )}
       <br />
       {queryInfo.isFetching ? 'Updating...' : null}
     </div>
