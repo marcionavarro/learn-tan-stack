a8abf14f91e4504c18b7e8e4faa22f23381e1db3
marcionavarro <marcionavarrodearaujo@gmail.com>
2024-02-02 00:15:51 -0300

    Querying Related Lists and Items

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,79 @@
-import { useQuery } from 'react-query'
+import { useQuery, QueryClient, QueryClientProvider } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools';
 import axios from 'axios';
-import './App.css'
+import { useState } from 'react';
 
 
-import existingUser from './existingUser'
-const email = 'Sincere@april.biz'
-
-function MyPosts() {
-  const userQuery = useQuery('user', async () => {
+function Posts({ setPostId }) {
+  const postsQuery = useQuery('posts', async () => {
     await new Promise(resolve => setTimeout(resolve, 1000))
-    return axios
-      .get(`https://jsonplaceholder.typicode.com/users?email=${email}`)
-      .then(res => res.data[0])
-  }, {
-    initialData: existingUser,
-    initialStale: true
+    return axios.get('https://jsonplaceholder.typicode.com/posts').then(res => res.data)
+
   })
 
-  return userQuery.isLoading ? (
-    'Loading user...'
-  ) : (
+  return (
     <div>
-      <pre>{JSON.stringify(userQuery.data, null, 2)}</pre>
-      {userQuery.isFetching ? 'Updating...' : null}
+      <h1>Posts {postsQuery.isFetching ? '...' : null}</h1>
+      {postsQuery.isLoading ? (
+        'Loading posts...'
+      ) : (
+        <div>
+          {postsQuery.data.map(post => {
+            return (
+              <div key={post.id}>
+                <a href='#' onClick={() => setPostId(post.id)}>
+                  {post.title}
+                </a>
+              </div>
+            )
+          })}
+        </div>
+      )}
     </div>
   )
 }
 
-function App() {
+function Post({ postId, setPostId }) {
+  const postQuery = useQuery(['posts', postId], async () => {
+    await new Promise(resolve => setTimeout(resolve, 1000));
+    return axios.get(`https://jsonplaceholder.typicode.com/posts/${postId}`).then(res => res.data)
+  })
   return (
     <div>
-      <MyPosts />
+      <a href='#' onClick={() => setPostId(-1)}>
+        Back
+      </a>
+      <br /><br />
+      {postQuery.isLoading ? 'Loading post...'
+        : (
+          <>
+            {postQuery.data.title}
+            <br /><br />
+            {postQuery.isFetching ? 'Updating...' : null}
+          </>
+        )}
     </div>
+  )
+}
+
+
+function App() {
+  const queryClient = new QueryClient();
+  const [postId, setPostId] = useState(-1)
+  return (
+    <div>
+      <QueryClientProvider client={queryClient}>
+
+        {postId > -1 ? (
+          <Post postId={postId} setPostId={setPostId} />
+        ) : (
+          <Posts setPostId={setPostId} />
+        )}
+
+
+        <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
+      </QueryClientProvider>
+    </div >
   )
 }
 
